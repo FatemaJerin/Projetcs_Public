bb256f50f42331d197bf36b8da62e5da491cc373
Jessica <Jessica@Jessica.myfiosgateway.com>
2022-09-20 11:07:14 -0400

    DemoClass java commiting

---
+++ Selenium_Project/src/DemoPackage/DemoClass.java
@@ -0,0 +1,18 @@
+package DemoPackage;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count =1;
+		// perform the loop operation using while condition
+		while(count<=10)
+		{
+			System.out.println("Name");
+			// Increase the value by one .The loop works till the condition is false 
+			count++;
+		}
+
+	}
+
+}
